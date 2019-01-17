7fae89ea2fd04cb9ea30d44f5dafe54647d55cfa
RyzeZhao <643080112@qq.com>
2019-01-17 15:54:20 +0800

    RabbitMQ-普通模式模块：初始化

---
+++ rabbitmq-normal/src/main/java/com/zhaolearn/rabbitmqnormal/RabbitmqNormalApplication.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.rabbitmqnormal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqNormalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqNormalApplication.class, args);
+    }
+
+}
+
