f8c9a4ff71d2fa4dda4dfc221ae8353e4eb2d1b3
RyzeZhao <643080112@qq.com>
2019-01-17 17:03:45 +0800

    RabbitMQ-工作模式模块：配置与创建

---
+++ rabbitmq-consumer/src/main/java/com/zhaolearn/rabbitmqconsumer/WorkConsumer.java
@@ -0,0 +1,25 @@
+package com.zhaolearn.rabbitmqconsumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 这个类作为消费者
+ *
+ * @author: HeHaoZhao
+ * @date: 2019/1/16 11:00
+ */
+@Component
+@RabbitListener(queues = "message.rabbit.queue.name")
+public class WorkConsumer {
+    private final static Logger LOGGER = LoggerFactory.getLogger(WorkConsumer.class);
+
+    @RabbitHandler
+    public void handler(String content) {
+        LOGGER.info("Work消费者消费读取Queue内容：{}", content);
+    }
+}
+
