de66863a9f14d6f3f02f390426531cdbce8e2efa
RyzeZhao <643080112@qq.com>
2019-01-18 14:09:07 +0800

    RabbitMQ-Topic模式模块：初始化

---
+++ rabbitmq-topic/src/main/java/com/zhaolearn/rabbitmqtopic/RabbitmqTopicApplication.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.rabbitmqtopic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqTopicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqTopicApplication.class, args);
+    }
+
+}
+
