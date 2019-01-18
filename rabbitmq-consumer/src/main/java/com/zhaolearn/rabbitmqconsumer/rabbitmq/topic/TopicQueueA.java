9536dfffb8bf6770ce14fe0f8fa7ff94f93b2da3
RyzeZhao <643080112@qq.com>
2019-01-18 15:02:22 +0800

    Queue消费者模块：更新

---
+++ rabbitmq-consumer/src/main/java/com/zhaolearn/rabbitmqconsumer/rabbitmq/topic/TopicQueueA.java
@@ -0,0 +1,25 @@
+package com.zhaolearn.rabbitmqconsumer.rabbitmq.topic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 这个类作为消费者
+ *
+ * @author: HeHaoZhao
+ * @date: 2019/1/16 11:00
+ */
+@Component
+@RabbitListener(queues = "message.rabbit.queue.topic.a")
+public class TopicQueueA {
+    private final static Logger LOGGER = LoggerFactory.getLogger(TopicQueueA.class);
+
+    @RabbitHandler
+    public void handler(String content) {
+        LOGGER.info("读取Queue内容：{}", content);
+    }
+}
+
