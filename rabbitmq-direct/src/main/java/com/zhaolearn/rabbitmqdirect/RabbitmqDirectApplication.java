2b71d22b426c38cbcdc0862511e32e13610a9eaf
RyzeZhao <643080112@qq.com>
2019-01-18 10:43:15 +0800

    RabbitMQ-Direct模式模块：配置与创建

---
+++ rabbitmq-direct/src/main/java/com/zhaolearn/rabbitmqdirect/RabbitmqDirectApplication.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.rabbitmqdirect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqDirectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqDirectApplication.class, args);
+    }
+
+}
+
---
+++ rabbitmq-direct/src/test/java/com/zhaolearn/rabbitmqdirect/RabbitmqDirectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zhaolearn.rabbitmqdirect;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqDirectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
