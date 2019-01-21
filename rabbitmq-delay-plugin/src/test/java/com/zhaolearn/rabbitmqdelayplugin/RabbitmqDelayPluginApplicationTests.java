bf57fc1c42a02aef67f580aedb02b9462fb0fd2b
RyzeZhao <643080112@qq.com>
2019-01-21 11:33:30 +0800

    RabbitMQ-Delay-Plugin模块(延期插件)：初始化

---
+++ rabbitmq-delay-plugin/src/test/java/com/zhaolearn/rabbitmqdelayplugin/RabbitmqDelayPluginApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zhaolearn.rabbitmqdelayplugin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqDelayPluginApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
