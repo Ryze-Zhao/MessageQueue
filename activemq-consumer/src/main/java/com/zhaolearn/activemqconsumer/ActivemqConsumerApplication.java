6b049bd13d55056d66de047487aab540f0f82bda
RyzeZhao <643080112@qq.com>
2019-01-21 17:25:10 +0800

    ActiveQueue消费者模块：初始化

---
+++ activemq-consumer/src/main/java/com/zhaolearn/activemqconsumer/ActivemqConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.activemqconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivemqConsumerApplication.class, args);
+    }
+
+}
+
