ebda285db1c2f0fb7b46d4325ec7e419c7bfbbf3
RyzeZhao <643080112@qq.com>
2019-01-21 17:34:09 +0800

    Active-Normal普通模块：初始化

---
+++ activemq-normal/src/main/java/com/zhaolearn/activemqnormal/ActivemqNormalApplication.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.activemqnormal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqNormalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivemqNormalApplication.class, args);
+    }
+
+}
+
