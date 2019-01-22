ab3ec32c462e1b3956e38255f488e87e10fcac94
RyzeZhao <643080112@qq.com>
2019-01-22 15:27:41 +0800

    Active-Topic订阅模式模块：初始化

---
+++ activemq-topic/src/main/java/com/zhaolearn/activemqtopic/ActivemqTopicApplication.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.activemqtopic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqTopicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivemqTopicApplication.class, args);
+    }
+
+}
+
