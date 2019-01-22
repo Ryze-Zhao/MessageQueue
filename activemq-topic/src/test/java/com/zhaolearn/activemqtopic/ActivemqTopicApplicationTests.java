06436208d70a4efc29ef8dbd5554acf7ef4aa4ad
RyzeZhao <643080112@qq.com>
2019-01-22 15:45:36 +0800

    Active-Topic订阅模式模块：创建与配置

--- activemq-topic/src/test/java/com/zhaolearn/activemqtopic/ActivemqTopicApplicationTests.java
+++ activemq-topic/src/test/java/com/zhaolearn/activemqtopic/ActivemqTopicApplicationTests.java
@@ -2,15 +2,25 @@ package com.zhaolearn.activemqtopic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ActivemqTopicApplicationTests {
 
+    @Autowired
+    private MessageProvider messageProvider;
     @Test
-    public void contextLoads() {
+    public void test() {
+        for(int i=0;i<5;i++){
+            String dateString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+            messageProvider.send(QueueEnum.MESSAGE_TOPIC_ENUM.getExchange(), "测试消费,写入时间：" + dateString);
+        }
     }
 
 }
