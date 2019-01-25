91790f8b9cb99949c0f749067786313e6280f266
RyzeZhao <643080112@qq.com>
2019-01-25 17:31:38 +0800

    ActiveMQ延时消息(Topic与Queue)模块：初始化

---
+++ activemq-delay/src/main/java/com/zhaolearn/activemqdelay/ActivemqDelayApplication.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.activemqdelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqDelayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivemqDelayApplication.class, args);
+    }
+
+}
+
---
+++ activemq-delay/src/test/java/com/zhaolearn/activemqdelay/ActivemqDelayApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zhaolearn.activemqdelay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivemqDelayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
