6f6474f1be24b962c700f39983804b1078f93685
RyzeZhao <643080112@qq.com>
2019-01-25 17:43:48 +0800

    ActiveMQ延时消息(Topic与Queue)模块：更新

--- activemq-delay/src/main/java/com/zhaolearn/activemqdelay/ScheduleMessagePostProcessor.java
+++ activemq-delay/src/main/java/com/zhaolearn/activemqdelay/ScheduleMessagePostProcessor.java
@@ -8,19 +8,14 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 
 public class ScheduleMessagePostProcessor implements MessagePostProcessor {
-
     private long delay = 0L;
-
     private String corn = null;
-
     public ScheduleMessagePostProcessor(long delay) {
         this.delay = delay;
     }
-
     public ScheduleMessagePostProcessor(String cron) {
         this.corn = cron;
     }
-
 
     @Override
     public Message postProcessMessage(Message message) throws JMSException {
