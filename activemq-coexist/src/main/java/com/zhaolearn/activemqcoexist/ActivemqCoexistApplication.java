466b93051a84e1ce209a39b9e83147ac5e141b8d
RyzeZhao <643080112@qq.com>
2019-01-25 16:43:01 +0800

    ActiveMQ同一模块可以同时收或发Topic与Queue消息:初始化

---
+++ activemq-coexist/src/main/java/com/zhaolearn/activemqcoexist/ActivemqCoexistApplication.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.activemqcoexist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqCoexistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivemqCoexistApplication.class, args);
+    }
+
+}
+
