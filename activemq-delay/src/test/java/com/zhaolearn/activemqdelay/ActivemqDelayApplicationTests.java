1ecfe2b55bee7cae9cd46f235d0602226d0d4138
RyzeZhao <643080112@qq.com>
2019-01-26 12:39:28 +0800

    ActiveMQ延时消息(Topic与Queue)模块：更新

--- activemq-delay/src/test/java/com/zhaolearn/activemqdelay/ActivemqDelayApplicationTests.java
+++ activemq-delay/src/test/java/com/zhaolearn/activemqdelay/ActivemqDelayApplicationTests.java
@@ -16,13 +16,38 @@ public class ActivemqDelayApplicationTests {
     private QueueProvider queueProvider;
     @Autowired
     private TopicProvider topicProvider;
+    //普通延时
     @Test
     public void testDelay(){
-        queueProvider.delaySend(QueueEnum.MESSAGE_QUEUE_ENUM.getQueueName(),10000,
-                "延时发送Queue消息"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-        topicProvider.delaySend(QueueEnum.MESSAGE_TOPIC_ENUM.getQueueName(),10000,
-                "延时发送Topic消息"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        queueProvider.delaySend(QueueEnum.MESSAGE_QUEUE_ENUM.getQueueName(),new ScheduleMessage(10000L),
+                "普通延时发送Queue消息"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        topicProvider.delaySend(QueueEnum.MESSAGE_TOPIC_ENUM.getQueueName(),new ScheduleMessage(10000L),
+                "普通延时发送Topic消息"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+    }
+    //corn定时
+    @Test
+    public void testDelay1(){
+        queueProvider.delaySend(QueueEnum.MESSAGE_QUEUE_ENUM.getQueueName(),new ScheduleMessage("29 * * * *"),
+                "corn定时发送Queue消息"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        topicProvider.delaySend(QueueEnum.MESSAGE_TOPIC_ENUM.getQueueName(),new ScheduleMessage("29 * * * *"),
+                "corn定时发送Topic消息"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
     }
 
+    //不延时重复发送，重复发送2次，每次间隔3秒
+    @Test
+    public void testDelay2(){
+        queueProvider.delaySend(QueueEnum.MESSAGE_QUEUE_ENUM.getQueueName(),new ScheduleMessage(3000,2),
+                "不延时重复发送Queue消息"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        topicProvider.delaySend(QueueEnum.MESSAGE_TOPIC_ENUM.getQueueName(),new ScheduleMessage(3000,2),
+                "不延时重复发送Topic消息"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+    }
+    //延时重复发送,延时10秒后，重复发送2次，每次间隔3秒
+    @Test
+    public void testDelay3(){
+        queueProvider.delaySend(QueueEnum.MESSAGE_QUEUE_ENUM.getQueueName(),new ScheduleMessage(10000,3000,2),
+                "延时重复发送Queue消息"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        topicProvider.delaySend(QueueEnum.MESSAGE_TOPIC_ENUM.getQueueName(),new ScheduleMessage(10000,3000,2),
+                "延时重复发送Topic消息"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+    }
 }
 
