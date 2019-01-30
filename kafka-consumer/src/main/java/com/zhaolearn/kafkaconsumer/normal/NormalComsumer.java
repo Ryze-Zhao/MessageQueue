3ef97576ab3d4662d7c1922b58d5e7bc40f37eb1
RyzeZhao <643080112@qq.com>
2019-01-30 17:37:48 +0800

    Kafka：Consumer模块：更新

--- kafka-consumer/src/main/java/com/zhaolearn/kafkaconsumer/normal/NormalComsumer.java
+++ kafka-consumer/src/main/java/com/zhaolearn/kafkaconsumer/normal/NormalComsumer.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class NormalComsumer {
     private final static Logger LOGGER = LoggerFactory.getLogger(NormalComsumer.class);
-    @KafkaListener(topics = {"normal-consumer-group-topic"}, groupId = "normal-consumer-group-id")
+    @KafkaListener(topics = {"normal_consumer_group_topic"}, groupId = "normal_consumer_group_id")
     public void listen(ConsumerRecord<?, ?> record) {
         LOGGER.info("NormalComsumer所有信息："+record.toString());
 //        LOGGER.info("【消费{}主题, 第{}分区, 数据:{}, 偏移量:{}】", record.topic(), record.partition(), record.value(), record.offset());
