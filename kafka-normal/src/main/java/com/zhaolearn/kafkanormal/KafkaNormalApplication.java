1672028ea3314afc4318c89ae725d3a5d45994f7
RyzeZhao <643080112@qq.com>
2019-01-30 17:23:58 +0800

    Kafka：Normal模块：初始化

---
+++ kafka-normal/src/main/java/com/zhaolearn/kafkanormal/KafkaNormalApplication.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.kafkanormal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaNormalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaNormalApplication.class, args);
+    }
+
+}
+
